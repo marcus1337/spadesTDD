a8e46b91a4cfa086738b59294ee9f69e99f5fba5
marcus1337 <blurgas@hotmail.com>
2023-10-21 22:08:49 +0200

    unit test randomizer

--- test/test0.cpp
+++ test/test0.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include "Spades.h"
 #include "global/PortableRandom.h"
+#include <algorithm>
 
 using namespace spd;
 
@@ -10,4 +11,43 @@ TEST(test0, HelloWorldAssert) {
   EXPECT_EQ(spades.getTestNumber(), 1337);
 }
 
+TEST(test1, TestRandomSame) {
+    auto& r = PortableRandom::getInstance();
+    std::vector<int> arr1, arr2;
+    const int n = 200;
+    const int maxValue = 100;
+    const int seed = 100;
 
+    r.setSeed(seed);
+    for (int i = 0; i < n; i++) {
+        arr1.push_back(r.randInt(0, maxValue));
+    }
+    r.setSeed(seed);
+    for (int i = 0; i < n; i++) {
+        arr2.push_back(r.randInt(0, maxValue));
+    }
+    bool areEqual = std::equal(arr1.begin(), arr1.end(), arr2.begin());
+
+    EXPECT_TRUE(areEqual);
+}
+
+TEST(test1, TestRandomDiff) {
+    auto& r = PortableRandom::getInstance();
+    std::vector<int> arr1, arr2;
+    const int n = 200;
+    const int maxValue = 100;
+    const int seed1 = 100;
+    const int seed2 = seed1+1;
+
+    r.setSeed(seed1);
+    for (int i = 0; i < n; i++) {
+        arr1.push_back(r.randInt(0, maxValue));
+    }
+    r.setSeed(seed2);
+    for (int i = 0; i < n; i++) {
+        arr2.push_back(r.randInt(0, maxValue));
+    }
+    bool areEqual = std::equal(arr1.begin(), arr1.end(), arr2.begin());
+
+    EXPECT_FALSE(areEqual);
+}
