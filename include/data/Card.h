a92579486f138fbd687f77cc49aa1584c98fafdf
marcus1337 <blurgas@hotmail.com>
2023-10-28 14:10:51 +0200

    refactor Card

--- include/data/Card.h
+++ include/data/Card.h
@@ -3,58 +3,130 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
-#include <iterator> 
+#include <iterator>
 #include <string>
+#include <variant>
+#include <array>
 
 namespace spd
 {
-    enum class Rank {
-        ACE = 0, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, KNIGHT, QUEEN, KING, BIG_JOKER, LITTLE_JOKER
+    enum class Rank
+    {
+        ACE = 0,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        KNIGHT,
+        QUEEN,
+        KING,
+        BIG_JOKER,
+        LITTLE_JOKER
     };
-    enum class Suit {
-        SPADE, HEART, DIAMOND, CLOVER, NONE
+    enum class Suit
+    {
+        SPADE,
+        HEART,
+        DIAMOND,
+        CLOVER,
+        NONE
     };
 
-    class Card {
+    enum class Joker
+    {
+        BIG,
+        LITTLE
+    };
+
+    class Card
+    {
         Rank rank;
         Suit suit;
+
     public:
-        
-        Card(const Rank& rank, const Suit& suit) : rank(rank), suit(suit) {
+        Card() : rank(Rank::ACE), suit(Suit::SPADE) {}
+        Card(const Rank &rank, const Suit &suit) : rank(rank), suit(suit)
+        {
             isJoker(rank) ? assert(suit == Suit::NONE) : assert(suit != Suit::NONE);
         }
-        static Card BigJoker() {
+        static Card BigJoker()
+        {
             return Card(Rank::BIG_JOKER, Suit::NONE);
         }
-        static Card LittleJoker() {
+        static Card LittleJoker()
+        {
             return Card(Rank::LITTLE_JOKER, Suit::NONE);
         }
-        static bool isJoker(const Rank& rank) {
+        static bool isJoker(const Rank &rank)
+        {
             return rank == Rank::BIG_JOKER || rank == Rank::LITTLE_JOKER;
         }
-        bool operator==(const Card& other) const {
+        bool operator==(const Card &other) const
+        {
             return sameRank(other) && sameSuit(other);
         }
-        bool operator!=(const Card& other) const {
+        bool operator!=(const Card &other) const
+        {
             return !(*this == other);
         }
-        bool operator<(const Card& other) const {
+        bool operator<(const Card &other) const
+        {
             return sameSuit(other) ? getRank() < other.getRank() : getSuit() < other.getSuit();
         }
-        Rank getRank() const {
+        Rank getRank() const
+        {
             return rank;
         }
-        Suit getSuit() const {
+        Suit getSuit() const
+        {
             return suit;
         }
-        bool sameSuit(const Card& other) const {
+        bool sameSuit(const Card &other) const
+        {
             return getSuit() == other.getSuit();
         }
-        bool sameRank(const Card& other) const {
+        bool sameRank(const Card &other) const
+        {
             return getRank() == other.getRank();
         }
-        std::string toString() const {
+        std::string toString() const
+        {
             return std::to_string((int)getRank()) + "," + std::to_string((int)getSuit());
+        }
+
+        static std::array<Suit, 4> getSuits()
+        {
+            return {Suit::SPADE, Suit::HEART, Suit::CLOVER, Suit::DIAMOND};
+        }
+        static std::array<Rank, 13> getRanks()
+        {
+            std::array<Rank, 13> ranks;
+            for (int i = 0; i < ranks.size(); i++)
+                ranks[i] = (Rank)i;
+            return ranks;
+        }
+
+        static std::vector<Card> getDefaultCards()
+        {
+            std::vector<Card> cards;
+            for (const auto &suit : getSuits())
+                for (const auto &rank : getRanks())
+                    cards.emplace_back(rank, suit);
+            return cards;
+        }
+
+        static std::vector<Card> getCards()
+        {
+            std::vector<Card> cards;
+            const auto defaultCards = getDefaultCards();
+            cards.insert(cards.end(), defaultCards.begin(), defaultCards.end());
+            cards.insert(cards.end(), {LittleJoker(), BigJoker()});
+            return cards;
         }
     };
 
--- include/table/Deck.h
+++ include/table/Deck.h
@@ -15,79 +15,34 @@ namespace spd
         PortableRandom portableRandom;
         std::array<Card, 2> excludeCards;
 
-        std::vector<Suit> getSuits() const
+        bool isExcluded(const Card &card) const
         {
-            return {spd::Suit::CLOVER, spd::Suit::DIAMOND, spd::Suit::HEART, spd::Suit::SPADE};
-        }
-
-        std::vector<Rank> getRanks() const
-        {
-            std::vector<Rank> ranks;
-            for (int i = 0; i < 15; i++)
-            {
-                ranks.push_back((Rank)i);
-            }
-            return ranks;
-        }
-
-        bool vectorContainsCard(const std::vector<Card> &cards, const Card &card) const
-        {
-            return std::find(cards.begin(), cards.end(), card) != cards.end();
-        }
-
-        std::vector<Card> getDefaultCards() const
-        {
-            std::vector<Card> cards;
-            for (const auto &suit : getSuits())
-            {
-                for (const auto &rank : getRanks())
-                {
-                    if (!Card::isJoker(rank))
-                    {
-                        cards.push_back(Card(rank, suit));
-                    }
-                }
-            }
-            return cards;
-        }
-        std::array<Card, 2> getJokers() const
-        {
-            return {Card::BigJoker(), Card::LittleJoker()};
-        }
-
-        void addCardsExcluding(std::vector<Card>& cards, const std::vector<Card> cardsToAdd) const
-        {
-            for (const auto &card : cardsToAdd)
-            {
-                if (excludeCards[0] != card && excludeCards[1] != card)
-                {
-                    cards.push_back(card);
-                }
-            }
+            for (const auto &excludedCard : excludeCards)
+                if (excludedCard == card)
+                    return true;
+            return false;
         }
 
         std::vector<Card> getCards() const
         {
             std::vector<Card> cards;
-            addCardsExcluding(cards, getDefaultCards());
-            const auto jokers = getJokers();
-            addCardsExcluding(cards, {jokers[0], jokers[1]});
+            for (const auto &card : Card::getCards())
+                if (!isExcluded(card))
+                    cards.push_back(card);
             return cards;
         }
 
         std::vector<Card> getShuffledCards(int round)
         {
-            std::vector<Card> cards = getCards();
             setSeed(getSeed());
+            std::vector<Card> cards = getCards();
             for (int i = 0; i <= round; i++)
-            {
                 portableRandom.shuffle(cards);
-            }
             return cards;
         }
 
     public:
-        Deck() : excludeCards(getJokers())
+        Deck() : excludeCards({Card::BigJoker(), Card::LittleJoker()})
         {
         }
 
@@ -97,18 +52,12 @@ namespace spd
             this->excludeCards = excludeCards;
         }
 
-        std::vector<Card> getHand(const Seat &seat, int round)
+        std::array<Card, 13> getHand(const Seat &seat, int round)
         {
-            const auto cards = getShuffledCards(round);
             const int handSize = 13;
             const int deckOffset = static_cast<int>(seat) * handSize;
-            std::vector<Card> hand;
-
-            auto start = std::begin(cards) + deckOffset;
-            auto end = std::begin(cards) + std::min(deckOffset + handSize, static_cast<int>(cards.size()));
-
-            std::copy(start, end, std::back_inserter(hand));
-            assert(hand.size() == handSize);
+            std::array<Card, 13> hand{};
+            std::copy_n(getShuffledCards(round).begin() + deckOffset, handSize, hand.begin());
             return hand;
         }
 
