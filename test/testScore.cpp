dc4b4fd135baef14b0263b9a88bda108a7564403
marcus1337 <blurgas@hotmail.com>
2023-11-12 07:39:43 +0100

    score test

--- test/testScore.cpp
+++ test/testScore.cpp
@@ -3,9 +3,22 @@
 
 using namespace spd;
 
-TEST(Score, storing) {
+TEST(Score, storing)
+{
     Spades spades;
     auto score = spades.getScore();
     EXPECT_EQ(score.first.getPoints(), 0);
     EXPECT_EQ(score.second.getPoints(), 0);
+    spades.addBid(1);
+    spades.addBid(1);
+    spades.addBid(1);
+    spades.addBid(1);
+    for (int i = 0; i < 52; i++)
+    {
+        spades.place(spades.getHand(spades.getTurnSeat()).front());
+    }
+    const auto bags1 = spades.getScore().first.getRoundBags();
+    const auto bags2 = spades.getScore().second.getRoundBags();
+    ASSERT_TRUE(bags1.size() == 1);
+    EXPECT_TRUE(bags1.front() + bags2.front() > 0);
 }
