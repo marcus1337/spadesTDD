9599449c107aa2dfdfc5d496fe2018841ba88712
marcus1337 <blurgas@hotmail.com>
2023-12-07 09:29:47 +0100

    refactor

--- include/spades_ai/AIObservation.h
+++ include/spades_ai/AIObservation.h
@@ -10,123 +10,22 @@ namespace spd
         const Spades &spades;
         const Analyze analyze;
 
-        bool canPlaceNonTopCard(const Seat &seat) const
-        {
-            for (const auto &card : spades.getHand(seat))
-            {
-                if (!spades.isTopCardIfPlaced(card))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        bool hasPlacedCard(const Seat &seat) const
-        {
-            for (const auto &[trickSeat, card] : spades.getCurrentTrickCardSeatPairs())
-            {
-                if (trickSeat == seat)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        bool isDefendingNil(const Seat &seat) const
-        {
-            const int numTakenTricks = spades.getNumberOfTakenTricksCurrentRound(seat);
-            const bool nilBid = spades.getBidResult(seat).value();
-            return nilBid && numTakenTricks == 0;
-        }
-
-        int getCountedRoundTricks(const Seat &seat) const
-        {
-            int bid = spades.getBidResult(seat).value_or(0);
-            if (bid == 0)
-            {
-                return 0;
-            }
-            else
-            {
-                return spades.getNumberOfTakenTricksCurrentRound(seat);
-            }
-        }
-
-        int getCountedRoundTeamTricks(const Seat &seat) const
-        {
-            const Seat teamSeat = SeatUtils::getTeamSeat(seat);
-            return getCountedRoundTricks(seat) + getCountedRoundTricks(teamSeat);
-        }
-
-        int getTeamBid(const Seat &seat) const
-        {
-            const Seat teamSeat = SeatUtils::getTeamSeat(seat);
-            return spades.getBidResult(seat).value_or(0) + spades.getBidResult(teamSeat).value_or(0);
-        }
+        bool isLastTrickCard() const;
+        bool canPlaceNonTopCard(const Seat &seat) const;
+        bool hasPlacedCard(const Seat &seat) const;
+        bool isDefendingNil(const Seat &seat) const;
+        int getCountedRoundTricks(const Seat &seat) const;
+        int getCountedRoundTeamTricks(const Seat &seat) const;
+        int getTeamBid(const Seat &seat) const;
 
     public:
-        AIObservation(const Spades &spades) : spades(spades), analyze(spades)
-        {
-        }
-
-        bool canSabotageOpponentNil() const
-        {
-            const auto seat = spades.getTurnSeat();
-            const auto rightOpponent = SeatUtils::getRightOpponentSeat(seat);
-            const auto leftOpponent = SeatUtils::getLeftOpponentSeat(seat);
-            const auto trickSeatCards = spades.getCurrentRoundCardSeatPairs();
-            if (trickSeatCards.size() == NUM_SEATS - 1)
-            {
-                for (const auto &opponent : {rightOpponent, leftOpponent})
-                {
-                    if (spades.getBidResult(opponent).value() == 0 && spades.getCurrentTrickTopSeat().value() == opponent)
-                    {
-                        return canPlaceNonTopCard(seat);
-                    }
-                }
-            }
-            return false;
-        }
-
-        bool isDefendingNil() const
-        {
-            return isDefendingNil(spades.getTurnSeat());
-        }
-        bool isDefendingTeamNil() const
-        {
-            return isDefendingNil(SeatUtils::getTeamSeat(spades.getTurnSeat()));
-        }
-        bool isOpponentDefendingNil() const
-        {
-            const auto seat = spades.getTurnSeat();
-            return isDefendingNil(SeatUtils::getLeftOpponentSeat(seat)) || isDefendingNil(SeatUtils::getRightOpponentSeat(seat));
-        }
-
-        bool needMoreTricks() const
-        {
-            const auto seat = spades.getTurnSeat();
-            const auto bid = spades.getBidResult(seat);
-            if (bid == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return bid > getCountedRoundTeamTricks(seat);
-            }
-        }
-
-        bool teamNeedMoreTricks() const
-        {
-            const auto seat = spades.getTurnSeat();
-            return getTeamBid(seat) > getCountedRoundTeamTricks(seat);
-        }
-        bool opponentNeedMoreTricks() const
-        {
-            const auto seat = SeatUtils::getLeftOpponentSeat(spades.getTurnSeat());
-            return getTeamBid(seat) > getCountedRoundTeamTricks(seat);
-        }
+        AIObservation(const Spades &spades);
+        bool canSabotageOpponentNil() const;
+        bool isDefendingNil() const;
+        bool isDefendingTeamNil() const;
+        bool isOpponentDefendingNil() const;
+        bool needMoreTricks() const;
+        bool teamNeedMoreTricks() const;
+        bool opponentNeedMoreTricks() const;
     };
 }
---
+++ src/spades_ai/AIObservation.cpp
@@ -0,0 +1,124 @@
+#include "spades_ai/AIObservation.h"
+
+using namespace spd;
+
+bool AIObservation::isLastTrickCard() const
+{
+    return spades.getCurrentTrickCardSeatPairs().size() == NUM_SEATS - 1;
+}
+
+bool AIObservation::canPlaceNonTopCard(const Seat &seat) const
+{
+    for (const auto &card : spades.getHand(seat))
+    {
+        if (!spades.isTopCardIfPlaced(card))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool AIObservation::hasPlacedCard(const Seat &seat) const
+{
+    for (const auto &[trickSeat, card] : spades.getCurrentTrickCardSeatPairs())
+    {
+        if (trickSeat == seat)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool AIObservation::isDefendingNil(const Seat &seat) const
+{
+    const int numTakenTricks = spades.getNumberOfTakenTricksCurrentRound(seat);
+    const bool nilBid = spades.getBidResult(seat).value();
+    return nilBid && numTakenTricks == 0;
+}
+
+int AIObservation::getCountedRoundTricks(const Seat &seat) const
+{
+    if (spades.getBidResult(seat).value_or(0) == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return spades.getNumberOfTakenTricksCurrentRound(seat);
+    }
+}
+
+int AIObservation::getCountedRoundTeamTricks(const Seat &seat) const
+{
+    const Seat teamSeat = SeatUtils::getTeamSeat(seat);
+    return getCountedRoundTricks(seat) + getCountedRoundTricks(teamSeat);
+}
+
+int AIObservation::getTeamBid(const Seat &seat) const
+{
+    const Seat teamSeat = SeatUtils::getTeamSeat(seat);
+    return spades.getBidResult(seat).value_or(0) + spades.getBidResult(teamSeat).value_or(0);
+}
+
+AIObservation::AIObservation(const Spades &spades) : spades(spades), analyze(spades)
+{
+}
+
+bool AIObservation::canSabotageOpponentNil() const
+{
+    const auto seat = spades.getTurnSeat();
+    const auto rightOpponent = SeatUtils::getRightOpponentSeat(seat);
+    const auto leftOpponent = SeatUtils::getLeftOpponentSeat(seat);
+    if (isLastTrickCard())
+    {
+        for (const auto &opponent : {rightOpponent, leftOpponent})
+        {
+            if (spades.getBidResult(opponent).value() == 0 && spades.getCurrentTrickTopSeat().value() == opponent)
+            {
+                return canPlaceNonTopCard(seat);
+            }
+        }
+    }
+    return false;
+}
+
+bool AIObservation::isDefendingNil() const
+{
+    return isDefendingNil(spades.getTurnSeat());
+}
+bool AIObservation::isDefendingTeamNil() const
+{
+    return isDefendingNil(SeatUtils::getTeamSeat(spades.getTurnSeat()));
+}
+bool AIObservation::isOpponentDefendingNil() const
+{
+    const auto seat = spades.getTurnSeat();
+    return isDefendingNil(SeatUtils::getLeftOpponentSeat(seat)) || isDefendingNil(SeatUtils::getRightOpponentSeat(seat));
+}
+
+bool AIObservation::needMoreTricks() const
+{
+    const auto seat = spades.getTurnSeat();
+    const auto bid = spades.getBidResult(seat);
+    if (bid == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return bid > getCountedRoundTeamTricks(seat);
+    }
+}
+
+bool AIObservation::teamNeedMoreTricks() const
+{
+    const auto seat = spades.getTurnSeat();
+    return getTeamBid(seat) > getCountedRoundTeamTricks(seat);
+}
+bool AIObservation::opponentNeedMoreTricks() const
+{
+    const auto seat = SeatUtils::getLeftOpponentSeat(spades.getTurnSeat());
+    return getTeamBid(seat) > getCountedRoundTeamTricks(seat);
+}
