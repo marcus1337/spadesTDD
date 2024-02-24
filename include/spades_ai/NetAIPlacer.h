133075f26b6309ada040f19dfc77e22e23052a63
marcus1337 <blurgas@hotmail.com>
2024-02-24 12:51:43 +0100

    ai

--- include/spades_ai/NetAIPlacer.h
+++ include/spades_ai/NetAIPlacer.h
@@ -39,14 +39,14 @@ namespace spd
         // bool in  (3): any opponent protects nil, team player protects nil, protecting nil
         // bool in  (12): player might have suit (suits*num_other_players)
         // bool in  (4): has_topcard_in_hand(#suits)
-        // bool in  (4): has_lowcard_in_hand(#suits)
-        // bool in  (4): lead_suit(#suits)
+        // bool in  (4): placed_lead_suit(#suits)
+        // bool in  (4): #num_placed_trick_cards
         // float in (4): percentage_of_remaining_cards_in_hand(#suits), example: out of all remaining cards of type #suit - how large perc. in my hand? (special case when no remaining cards: input is 0)
         // float in (4): percentage_of_remaining_cards(#suits), example: out of all remaining cards how large percentage is of type #suit?
 
         // bool  out (4): place suit if possible {TRUMP,D,H,C}
         // bool  out (1): try win current trick?
-        // float out (1): Card strength?(0..1) - {TRUMP,D,H,C}
+        // float out (1): Suit card strength?(0..1), strength of the subset of cards that are either losing or winning current trick
         neuralnet::NeuralNet network;
 
         bool tryWinLeadSuit(const Spades &spades)
