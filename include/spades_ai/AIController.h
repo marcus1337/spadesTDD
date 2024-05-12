a9a7d904a1949aeb592a5525e0d1ccca7c5250a7
marcus1337 <blurgas@hotmail.com>
2024-05-12 17:11:11 +0200

    ai placer

--- include/spades_ai/AIController.h
+++ include/spades_ai/AIController.h
@@ -16,9 +16,10 @@ namespace spd
     class AIController
     {
         std::shared_ptr<Spades> spades;
-        NetAIPlacer atkAI, defAI;
         RandomAIPlacer randAI;
         ZeroAIPlacer zeroPlacer;
+        SabotageZeroAIPlacer sabotageZeroPlacer;
+        DefendZeroAIPlacer defendZeroPlacer;
         AIStrategy strategy = AIStrategy::ATTACK;
 
         std::string loadText(const std::string &filepath) const
@@ -36,20 +37,31 @@ namespace spd
 
         AIPlacer &getPlacer()
         {
-            const auto &seat = spades->getTurnSeat();
-            const auto &numTakenTricks = spades->getNumberOfTakenTricksCurrentRound(seat);
-            const auto &bid = spades->getBidResult(seat).value();
+            const auto seat = spades->getTurnSeat();
+            const auto teamSeat = SeatUtils::getTeamSeat(seat);
+            const auto opp1 = SeatUtils::getLeftOpponentSeat(seat);
+            const auto opp2 = SeatUtils::getRightOpponentSeat(seat);
+            const auto numTakenTricks = spades->getNumberOfTakenTricksCurrentRound(seat);
+            const auto numTakenTricksTeamPlayer = spades->getNumberOfTakenTricksCurrentRound(teamSeat);
+            const auto bid = spades->getBidResult(seat).value();
+            const auto teamBid = spades->getBidResult(teamSeat).value();
+            const auto opp1Bid = spades->getBidResult(opp1).value();
+            const auto opp2Bid = spades->getBidResult(opp2).value();
+            const auto numTricksOpp1 = spades->getNumberOfTakenTricksCurrentRound(opp1);
+            const auto numTricksOpp2 = spades->getNumberOfTakenTricksCurrentRound(opp2);
+            const bool enemyIsDefendingNil = (opp1Bid == 0 && numTricksOpp1 == 0) || (opp2Bid == 0 && numTricksOpp2 == 0);
+
             if (bid == 0 && numTakenTricks == 0)
             {
                 return zeroPlacer;
             }
-            else if (strategy == AIStrategy::ATTACK)
+            else if (teamBid == 0 && numTakenTricksTeamPlayer == 0)
             {
-                return atkAI;
+                return defendZeroPlacer;
             }
-            else if (strategy == AIStrategy::DEFEND)
+            else if (enemyIsDefendingNil)
             {
-                return defAI;
+                return sabotageZeroPlacer;
             }
             else
             {
@@ -79,13 +91,13 @@ namespace spd
 
         bool load(const std::string &filepath, const AIStrategy &strategy)
         {
-            switch (strategy)
+/*             switch (strategy)
             {
             case AIStrategy::ATTACK:
                 return atkAI.deserialize(loadText(filepath));
             case AIStrategy::DEFEND:
                 return defAI.deserialize(loadText(filepath));
-            }
+            } */
             return false;
         }
 
