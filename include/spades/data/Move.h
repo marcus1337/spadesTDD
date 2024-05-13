f0f1c5c712114f1287867503a4e2f9d8553c6d87
marcus1337 <blurgas@hotmail.com>
2024-05-13 17:23:00 +0200

    sort of fix?

--- include/spades/data/Move.h
+++ include/spades/data/Move.h
@@ -23,10 +23,10 @@ namespace spd
         spd::Seat seat = spd::Seat::SOUTH;
 
     private:
-        static constexpr std::string BID_KEY = "M_BID";
-        static constexpr std::string BIDOPT_KEY = BID_KEY + "OPT";
-        static constexpr std::string CARD_KEY = "M_CARD";
-        static constexpr std::string SEAT_KEY = "M_SEAT";
+        static constexpr const char* BID_KEY = "M_BID";
+        static constexpr const char* BIDOPT_KEY = "M_BIDOPT";
+        static constexpr const char* CARD_KEY = "M_CARD";
+        static constexpr const char* SEAT_KEY = "M_SEAT";
         std::optional<spd::Card> deserializeCard(const std::string &encoding) const;
         std::optional<unsigned int> deserializeBid(const std::string &encoding) const;
         std::optional<spd::BidOption> deserializeBidOption(const std::string &encoding) const;
