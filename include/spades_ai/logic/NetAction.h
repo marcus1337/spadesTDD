0828fb1c9bc1c15545dfb791643dd005ad543a47
marcus1337 <blurgas@hotmail.com>
2024-05-16 22:03:19 +0200

    refactor action

--- include/spades_ai/logic/NetAction.h
+++ include/spades_ai/logic/NetAction.h
@@ -9,7 +9,8 @@ namespace spd
         static constexpr auto OUTPUT_SIZE = 52; // possible cards
         Card card = Card(0);
 
-        std::vector<unsigned int> getIndicesSortedByMax(const std::array<float, OUTPUT_SIZE> &output);
+        std::vector<unsigned int> getIndicesSortedByMax(const std::array<float, OUTPUT_SIZE> &output) const;
+        Card selectCard(const Spades &spades, const std::array<float, OUTPUT_SIZE> &output) const;
 
     public:
         NetAction(const Spades &spades, const std::array<float, OUTPUT_SIZE> &output);
--- src/spades_ai/logic/NetAction.cpp
+++ src/spades_ai/logic/NetAction.cpp
@@ -6,6 +6,11 @@ using namespace net;
 
 NetAction::NetAction(const Spades &spades, const std::array<float, OUTPUT_SIZE> &output)
 {
+    card = selectCard(spades, output);
+}
+
+Card NetAction::selectCard(const Spades &spades, const std::array<float, OUTPUT_SIZE> &output) const
+{
     const auto &allCards = spades.getStartCards();
     std::set<Card> handSet;
     for (const auto &card : spades.getHand(spades.getTurnSeat()))
@@ -17,10 +22,11 @@ NetAction::NetAction(const Spades &spades, const std::array<float, OUTPUT_SIZE> 
         const auto &indexCard = allCards[index];
         if (handSet.contains(indexCard) && spades.canPlace(indexCard))
         {
-            card = indexCard;
-            break;
+            return indexCard;
         }
     }
+    assert(false);
+    return Card(0);
 }
 
 Card NetAction::getPlacement() const
@@ -28,7 +34,7 @@ Card NetAction::getPlacement() const
     return card;
 }
 
-std::vector<unsigned int> NetAction::getIndicesSortedByMax(const std::array<float, OUTPUT_SIZE> &output)
+std::vector<unsigned int> NetAction::getIndicesSortedByMax(const std::array<float, OUTPUT_SIZE> &output) const
 {
     std::vector<std::pair<float, unsigned int>> indexedValues;
     for (unsigned int i = 0; i < OUTPUT_SIZE; ++i)
