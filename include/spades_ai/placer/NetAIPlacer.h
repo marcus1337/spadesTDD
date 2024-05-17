3a3b901cef7db7febe1b19c49b71949078eddfd9
marcus1337 <blurgas@hotmail.com>
2024-05-17 10:59:41 +0200

    ai

--- include/spades_ai/placer/NetAIPlacer.h
+++ include/spades_ai/placer/NetAIPlacer.h
@@ -13,33 +13,11 @@ namespace spd
     class NetAIPlacer : public AIPlacer, public Serializable
     {
     public:
-        NetAIPlacer() : network(Observation::OBSERVATION_SIZE, (Observation::OBSERVATION_SIZE + Action::OUTPUT_SIZE) / 2, Action::OUTPUT_SIZE)
-        {
-        }
-        virtual Card getPlacement(const Spades &spades) override
-        {
-            Observation observation(spades);
-            const auto &observationValues = observation.getValues();
-            const auto &outputValues = network.getOutput(std::vector<float>(observationValues.begin(), observationValues.end()));
-            std::array<float, Action::OUTPUT_SIZE> arrOutputValues{};
-            std::copy(outputValues.begin(), outputValues.end(), arrOutputValues.begin());
-            Action action(spades, arrOutputValues);
-            return action.getPlacement();
-        }
-        virtual std::string serialize() const override
-        {
-            return network.encode();
-        }
-        virtual bool deserialize(const std::string &encoding) override
-        {
-            network = neuralnet::NeuralNet(encoding);
-            return network.getNumLayers() == 3;
-        }
-
-        void mutate()
-        {
-            network.randomizeWeightSubset();
-        }
+        NetAIPlacer();
+        virtual Card getPlacement(const Spades &spades) override;
+        virtual std::string serialize() const override;
+        virtual bool deserialize(const std::string &encoding) override;
+        void mutate();
 
     private:
         neuralnet::NeuralNet network;
---
+++ src/spades_ai/NetAIPlacer.cpp
@@ -0,0 +1,31 @@
+#include "spades_ai/placer/NetAIPlacer.h"
+
+using namespace spd;
+
+NetAIPlacer::NetAIPlacer() : network(Observation::OBSERVATION_SIZE, (Observation::OBSERVATION_SIZE + Action::OUTPUT_SIZE) / 2, Action::OUTPUT_SIZE)
+{
+}
+Card NetAIPlacer::getPlacement(const Spades &spades)
+{
+    Observation observation(spades);
+    const auto &observationValues = observation.getValues();
+    const auto &outputValues = network.getOutput(std::vector<float>(observationValues.begin(), observationValues.end()));
+    std::array<float, Action::OUTPUT_SIZE> arrOutputValues{};
+    std::copy(outputValues.begin(), outputValues.end(), arrOutputValues.begin());
+    Action action(spades, arrOutputValues);
+    return action.getPlacement();
+}
+std::string NetAIPlacer::serialize() const
+{
+    return network.encode();
+}
+bool NetAIPlacer::deserialize(const std::string &encoding)
+{
+    network = neuralnet::NeuralNet(encoding);
+    return network.getNumLayers() == 3;
+}
+
+void NetAIPlacer::mutate()
+{
+    network.randomizeWeightSubset();
+}
